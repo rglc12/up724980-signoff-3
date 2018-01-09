611ca1d07d293dd9efb6f1621aebc38a0ae040f4
RyanLC <rglc12@gmail.com>
2018-01-09 15:20:29 +0000

    Initial Commit

---
+++ api/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const GoogleAuth = require('simple-google-openid');
+
+const api = express.Router();
+
+api.use(GoogleAuth(511406985315-3rk44gqt9ri26bkkchmi1um20ielribc.apps.googleusercontent.com));
+
+api.use('*', GoogleAuth.guardMiddleware({ realm: 'jwt' }));
+
+api.get('/random', async (req, res) =>{
+
+    if(req.user.displayName){
+
+        res.send(Math.random());
+
+    } else {
+
+        res.sendStatus(401);
+
+    }
+
+})
+
+module.exports = api;
